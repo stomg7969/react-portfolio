0c080563d0c1f94d57d93c57cacce93daeab1445
stomg7969 <stomg7969@gmail.com>
2019-07-18 00:38:13 -0400

    Test commit for AWS CodePipeline

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
   return (
     <footer>
       <p>my social media link here? including resume</p>
-      <p>scroll to the top button</p>
+      <p>scroll to the top button..</p>
       <h4>{data.site.siteMetadata.author}, © 2019</h4>
     </footer>
   );
